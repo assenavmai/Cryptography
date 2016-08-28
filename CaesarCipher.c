e915fcfb835dd7296ac020445bf3b818cf69cc5d
Vanessa White <assenavmai@users.noreply.github.com>
2016-08-28 11:03:36 -0400

    Finished encryption

--- CaesarCipher.c
+++ CaesarCipher.c
@@ -5,25 +5,27 @@
 
 #define MAXSIZE 1024
 
-void encrypt(char * str);
+void encrypt(char * str, int shift);
 
 int main(int argc, char const *argv[])
 {
-	char plaintext[] = "z";
+	char plaintext[] = "attack at dawn";
 
 	int i = 0;
 
 	printf("%s\n", plaintext);
 
-	encrypt(plaintext);
+	encrypt(plaintext, 5);
 
 
 	return 0;
 }
 
-void encrypt(char * str) {
+void encrypt(char * str, int shift) {
 
 	int i = 0;
+	int diff = 0;
+	int alphaStart = 97;
 	char *ptr = str;
 	char ciphertext[MAXSIZE];
 
@@ -31,7 +33,17 @@ void encrypt(char * str) {
 	{
 		tolower(*ptr);
 
-		ciphertext[i] = (*ptr + (char)1);
+		if(*ptr == ' ')
+			ciphertext[i] = *ptr;
+		else
+			ciphertext[i] = (*ptr + (char)shift);
+
+		if(ciphertext[i] > 122)
+		{
+			diff = ciphertext[i] - 122 - 1;
+			printf("%d\n", diff);
+			ciphertext[i]  = (char)alphaStart + (char)diff;
+		}
 
 		i++;
 		ptr++;
